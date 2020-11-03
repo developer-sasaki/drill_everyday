96040090dea1e011ca6eea00ad69773ea2c474fa
developer-sasaki <69506440+developer-sasaki@users.noreply.github.com>
2020-11-03 20:33:51 +0900

    ruby入門１

---
+++ 20201103.rb
@@ -0,0 +1,41 @@
+puts "hello world"
+puts "hello world2"
+puts "hello world3"
+
+=begin
+puts "hello world4"
+puts "hello world5"
+puts "hello world6"
+=end
+
+#puts "勇者は、荒野を歩いていた"
+puts "モンスターがあらわれた"
+
+
+puts "荒野を歩いていた"
+#puts "A:モンスターがあらわれた"
+puts "すごい剣を手にいれた"
+#puts "A:ドラゴンがあらわれた"
+#puts "A:魔王があらわれた"
+puts "世界を救った"
+
+number=rand(1..100)*100
+puts number
+
+number = (1234+5678)*3
+puts number 
+
+number = rand(1..10)
+money =  500 * number
+puts "スライムが#{number}匹あらわれた"
+puts "スライムを倒したときの獲得金額は#{money}円です"
+
+x = 50
+puts x - 10
+
+a = "モンスターが"
+b = "あらわれた"
+puts a+b
+
+
+
